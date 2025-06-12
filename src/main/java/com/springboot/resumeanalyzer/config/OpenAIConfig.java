8826f7fd94afeb302af88a5ce456b8c80d1db5c9
Neilchen863 <153107332+Neilchen863@users.noreply.github.com>
2025-06-12 20:58:04 +0800

    Use deepseek api

--- src/main/java/com/springboot/resumeanalyzer/config/OpenAIConfig.java
+++ src/main/java/com/springboot/resumeanalyzer/config/OpenAIConfig.java
@@ -1,17 +1,18 @@
 package com.springboot.resumeanalyzer.config;
 
 import com.theokanning.openai.service.OpenAiService;
-import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenAIConfig {
 
+    @Value("${DEEPSEEK_API_KEY}")
+    private String apiKey;
+
     @Bean
     public OpenAiService openAiService() {
-        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-        String openaiApiKey = dotenv.get("OPENAI_API_KEY");
-        return new OpenAiService(openaiApiKey);
+        return new OpenAiService(apiKey);
     }
 } 
